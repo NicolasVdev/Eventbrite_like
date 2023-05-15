a8384ea0625ed7869255078c3c6e35fff54dcda3
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-05-15 18:03:05 +0200

    final push v7

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   
   has_many :attendances
   has_many :events, through: :attendances
-  has_many :events, foreign_key: administrator_id, class_name: "Event"
+  has_many :events, foreign_key: "administrator_id", class_name: "Event"
 
   def administrator?
     events.count > 0
