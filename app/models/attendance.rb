4e7a1acd8ee324e49515661e4f7e6f5f75ea2c1c
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-05-15 16:52:46 +0200

    final push hope so!

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -1,4 +1,8 @@
 class Attendance < ApplicationRecord
   belongs_to :user
   belongs_to :event
+
+  def welcome_send
+    AttendanceMailer.welcome_attendance_email(self).deliver_now
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-# Seed pour créer des utilisateurs avec des adresses @yopmail.com
+User.destroy_all
+Event.destroy_all
+Attendance.destroy_all
 
 # Générer une adresse email aléatoire avec le domaine @yopmail.com
 def generate_yopmail_email
@@ -23,3 +18,26 @@ end
     last_name: Faker::Name.last_name
   )
 end
+puts 'Utilisateurs créés avec succès.'
+
+# Création des événements
+3.times do
+  Event.create(
+    start_date: Faker::Time.between_dates(from: Date.today, to: Date.today + 30),
+    title: Faker::Lorem.sentence,
+    location: Faker::Address.full_address,
+    description: Faker::Lorem.paragraph,
+    price: Faker::Number.between(from: 1, to: 1000),
+    duration: rand(1..100)*5,
+    administrator_id: User.all.sample.id
+  )
+end
+
+puts 'Événements créés avec succès.'
+
+# Création des participations
+5.times do
+  Attendance.create(stripe_customer_id: Faker::Alphanumeric.alphanumeric(number: 10), user_id: User.all.sample.id, event_id: Event.all.sample.id)
+end
+
+puts 'Participations créées avec succès.'
