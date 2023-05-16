39852310c8d164362db833fe9779943ff02d733a
NicolasVdev <nicolasvadelorge.dev@gmail.com>
2023-05-16 18:59:36 +0200

    nouvelles modifs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,26 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user
+  before_action :authenticate_administrator
+
   def show
     @user = User.find(params[:id])
   end
 end
+
+
+
+private
+
+  def authenticate_user
+    unless current_user
+      flash[:alert] = "Merci de vous connecter !"
+      redirect_to new_user_session_path
+    end
+  end
+
+  def authenticate_administrator
+    unless current_user == User.find(params[:id])
+      flash[:alert] = "Vous ne pouvez accéder qu'à votre profil"
+      redirect_to root_path
+    end
+  end
